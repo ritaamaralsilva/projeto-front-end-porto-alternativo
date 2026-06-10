765712774c02d4c898b62754e41b723f965c243f
ritaamaralsilva <rita.asilva93@gmail.com>
2026-06-10 15:06:00 +0100

    criacao de DB porto_alternativo em my sql e cri acao de pasta db com ficheiro Database.php (PDO) com nome da DB, username e password protegidos em .env de forma a nao p artilhar info sensivel

---
+++ db/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = 'localhost';
+$dbname = 'porto_alternativo';
+$username = $_ENV['DB_USERNAME'] ?? 'root';
+$password = $_ENV['DB_PASSWORD'] ?? '';
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na ligação à base de dados: " . $e->getMessage());
+}
